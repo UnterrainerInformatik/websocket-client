93fd5d17ed230b32f145b44f53db2d7b86bec5ae
guFalcon <gu_falcon@hotmail.com>
2025-07-25 15:42:24 +0200

    fix dependency issues

---
+++ src/main/java/info/unterrainer/commons/websocketclient/Information.java
@@ -0,0 +1,7 @@
+package info.unterrainer.commons.websocketclient;
+
+public class Information {
+	public static final String name = "Websocket-Client";
+	public static final String buildTime = "2025-07-25T12:38:56Z";
+	public static final String pomVersion = "1.0.0";
+}
