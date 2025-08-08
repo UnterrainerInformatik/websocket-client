6ca274a16cde1950bc272e102955112b3472c95f
Gerald <gerald@unterrainer.info>
2025-08-08 08:18:45 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/info/unterrainer/commons/websocketclient/Information.java
+++
@@ -1,7 +0,0 @@
-package info.unterrainer.commons.websocketclient;
-
-public class Information {
-	public static final String name = "Websocket-Client";
-	public static final String buildTime = "2025-08-07T14:26:04Z";
-	public static final String pomVersion = "1.0.3";
-}
